5318d6d07803c4004e84f49cc5f6af451ec2c4cd
ruixj <ustb_ruixj@aliyun.com>
2017-06-04 10:11:22 +0800

    Update wx.php

--- wx/wx.php
+++ wx/wx.php
@@ -3,7 +3,7 @@
 // 中文
 require_once( XRUI_PLUGIN_DIR . 'wx/redisjssdk.php' );
  
-$jssdk = new JSSDK("wx94d7f781c80eeb2b", "209062295c281505026e5fbbfcd309c2");
+$jssdk = new JSSDK("yourappid", "yourappkey");
 $signPackage = $jssdk->GetSignPackage();
 
 ?>
